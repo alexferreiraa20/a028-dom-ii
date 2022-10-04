a44ce9374ee9bce1b969c29087b5a72ce2009a38
Alex <alexferreiraa20@gmail.com>
2022-10-04 19:40:22 -0300

    check

---
+++ script.js
@@ -0,0 +1,13 @@
+function adicionaItem(event){
+    let elementoNovo = document.createElement("article")
+    elementoNovo.classList.add("item")
+    elementoNovo.onclick = removeItem;
+    let referencia = document.getElementById("container")
+    referencia.insertAdjacentElement("beforeend", elementoNovo)
+
+}
+
+
+function removeItem(event){
+    event.target.remove()
+}
